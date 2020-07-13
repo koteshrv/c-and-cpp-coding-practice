17ce0534236c612b1cef3594a04c39f2db847a8b
Kotesh <59207603+koteshrv@users.noreply.github.com>
2020-07-13 14:24:01 +0530

    Chef in Fantasy League

--- Codechef/FFL.cpp
+++ Codechef/FFL.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h> 
 using namespace std; 
  
-
 int main() 
 { 
     int t; 
@@ -28,10 +27,8 @@ int main()
                 }
             }
         }
-        if(flag == 1)
-            cout << "yes" <<endl;
-        else
-            cout << "no" <<endl;
+        if(flag == 1)  cout << "yes" <<endl;
+        else  cout << "no" <<endl;
         
     } 
     return 0; 
