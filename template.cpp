b75e81abc1dc058ac08751ee8b33bd3182946961
Kotesh <kotesh.rv@gmail.com>
2020-11-09 17:57:31 +0530

    added template

--- template.cpp
+++ template.cpp
@@ -29,17 +29,13 @@ typedef vector<vl>		vvl;
 
 //===================================================================================
 
-void solve() {
-
-}
-
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
     int t = 1;
     cin >> t;
     while(t--) {
-      solve();
+      
     }
 
     return 0;
