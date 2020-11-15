1945816f6c1d9def022c770e5266c2eb0d55c898
Kotesh <kotesh.rv@gmail.com>
2020-11-15 23:16:16 +0530

    Practice

---
+++ Hackerrank/Algorithms/Dayoftheprogrammer.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+// Complete the dayOfProgrammer function below.
+string dayOfProgrammer(int year) {
+    string s = "12.09.";
+    s+=to_string(year);
+    if(!(year%400==0||(year%4==0&&year%100!=0)))    s[1]='3';
+    if(year<=1917&&(year%400==0||(year%4==0&&year%100==0)))    s[1]='2';
+    if(year==1918)  {
+        s[0]='2';
+        s[1]='6';
+    }
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string year_temp;
+    getline(cin, year_temp);
+
+    int year = stoi(ltrim(rtrim(year_temp)));
+
+    string result = dayOfProgrammer(year);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
