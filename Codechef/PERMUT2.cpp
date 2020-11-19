fe84a5f1f2bd3722e7206e3726156ca6a096584d
Kotesh <kotesh.rv@gmail.com>
2020-11-19 11:35:12 +0530

    Practice

---
+++ Codechef/PERMUT2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    while(1){
+        int n;
+        cin >> n;
+        if(n==0)    break;
+        vector<int> a(n),rev(n);
+        for(int i=0;i<n;i++)    cin >> a[i];
+        for(int i=0;i<n;i++) rev[a[i]-1]=i+1;
+        if(a==rev)  cout << "ambiguous" << endl;
+        else cout << "not ambiguous" << endl;
+    }
+	return 0;
+}
