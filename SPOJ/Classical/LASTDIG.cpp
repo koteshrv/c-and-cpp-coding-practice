cbb735e7d8be129511485dffd7b357963b1b4ddc
Kotesh <kotesh.rv@gmail.com>
2020-12-20 02:08:30 +0530

    Practice

--- SPOJ/Classical/LASTDIG.cpp
+++ SPOJ/Classical/LASTDIG.cpp
@@ -5,20 +5,25 @@ int main() {
 
     int t;
     cin >> t;
-
     while(t--) {
         long long a, b;
         cin >> a >> b;
-        a %= 10;
-        if(b == 0)  cout << 1 << endl;
-        else if(a == 0) cout << 0 << endl;
+        if(a == 0 && b == 0)    cout << 0 << endl;
+        else if(b == 0)  cout << 1 << endl;
         else {
-            if(a == 2 || a == 3 || a == 7 || a == 8)    b %= 4;
-            else if (a == 4 || a == 9)  b %= 2;
+            a %= 10;
+            if(a == 2 || a == 3 || a == 7 || a == 8)    {
+                b %= 4;
+                if(b == 0)  b = 4;
+            }
+            else if (a == 4 || a == 9)  {
+                b %= 2;
+                if(b == 0)  b = 2;
+            }
             else b = 1;
+            int ans = pow(a, b);
+            cout << ans % 10 << endl;
         }
-        cout << int(pow(a, b)) % 10 << endl;
     }
-
     return 0;
 }
