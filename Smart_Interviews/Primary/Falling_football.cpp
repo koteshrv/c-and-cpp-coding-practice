3b9612aaaf8092542d42ae3f5da325f6c19004a1
Kotesh <kotesh.rv@gmail.com>
2021-01-03 02:07:31 +0530

    Practice

--- Smart_Interviews/Primary/Falling_football.cpp
+++ Smart_Interviews/Primary/Falling_football.cpp
@@ -11,36 +11,50 @@ int main() {
         cin >> n;
         vector<int> vec(n);
         for(int i = 0; i < n; i++) cin >> vec[i];
-        int max = *max_element(vec.begin(), vec.end());
-        int rows = 1, cols = max;
-        string str(cols + 1, '.');
-        vector<string> matrix(rows, str);
-        for(int i = 0; i < n; i++) {
-            if(matrix[rows][vec[i]] == '.') matrix[rows][vec[i]] = 'O';
-            else {
-                matrix.resize(++rows, str);
-                if(matrix[rows - 1][vec[i] + 1] == 'O' && matrix[rows - 1][vec[i] - 1] == 'O' && vec[i - 1] >= 0 && vec[i + 1] <= cols) 
-                    matrix[rows][vec[i]] = 'O';
+        vector<vector<char>> v(5, vector<char> (20, '.'));
+        int rows = 1;
 
-                else if(vec[i] == 0) {
-                    //if(matrix[rows - 1][vec[i] + 1] == 'O') matrix[rows - 1].insert()
-                }
+        for(int i = 0; i < n; i++) {
+            if(v[rows][vec[i]] == '.') v[rows][vec[i]] = 'O';
+
+            else if(v[rows][vec[i]] == 'O' && v[rows][vec[i] + 1] == '.' && 
+                    v[rows][vec[i] - 1] == '.' && vec[i] - 1 >= 0 && vec[i] + 1 < n)
+
+                    v[rows][vec[i] + 1] = 'O';
+
+            else if(v[rows][vec[i]] == 'O' && v[rows][vec[i] + 1] == 'O' 
+                    && v[rows][vec[i] - 1] == '.' && vec[i] - 1 >= 0 && vec[i] + 1 < n)
+
+                    v[rows][vec[i] - 1] = 'O';
+
+            else if(v[rows][vec[i]] == 'O' && v[rows][vec[i] + 1] == '.' 
+                    && v[rows][vec[i] - 1] == 'O' && vec[i] - 1 >= 0 && vec[i] + 1 < n)
+
+                    v[rows][vec[i] + 1] = 'O';
+
+            else if(v[rows][vec[i]] == 'O' && v[rows][vec[i] + 1] == 'O' && vec[i] == 0) {
+                    v.resize(rows, vector<char> (++cols));
+                auto it = v[rows].insert(v[rows].begin(), 'O');
             }
+
+            else if(v[rows][vec[i]] == 'O' && v[rows][vec[i] - 1] == 'O' && vec[i] == cols) {
+                    v.resize(rows, vector<char> (++cols, '.'));
+                v[rows].push_back('O');
+            }
+
+            else if(v[rows][vec[i]] == 'O' && v[rows][vec[i] + 1] == 'O' 
+                    && v[rows][vec[i] - 1] == 'O' && vec[i] - 1 >= 0 && vec[i] + 1 < n) {
+                        v.resize(++rows, vector<char> (cols, '.'));
+                        v[rows][vec[i]] = 'O';
+            }    
+        }*/
+        for(int i = v.size() - 1; i >= 0; i--) {
+            for(int j = 0; j < v[rows].size(); j++) {
+                cout << v[i][j];
+            }
+            cout << endl;
         }
     }
 
     return 0;
 }
-
-
-/*
-string s(10, '.');
-	int rows = 1;
-	vector<string> str(rows, s);
-	str[0][0] = 'O';
-	str.resize(++rows, s);
-	int len = str[rows].size() + 1;
-	str[rows].resize(len, '.');
-	str[rows].insert(0, 'O');
-	for(int i = 0; i < str.size(); i++) cout << str[i] << endl;
-*/
