e38dc9f3c9945e63c59da6a2c802fe179f693704
Kotesh <kotesh.rv@gmail.com>
2021-02-04 00:58:34 +0530

    practice

--- Smart_Interviews/Primary/Express_as_a_power_b.cpp
+++ Smart_Interviews/Primary/Express_as_a_power_b.cpp
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string check(int n) {
-    if(n == 1) return "Yes";
-    for(int i = 2; i * i <= n; i++) {
-        double val = log(n) / log(i);
-        if(val - int(val) < 0.000000001) return "Yes";
-    }
-    return "No";
+#define MAX 100000001
+bool ar[MAX] = {false};
+
+void apowb() {
+    for(int p = 2; p * p <= MAX; p++) {
+        if(ar[p] == false) {
+            for(long long i = p * p; i <= MAX; i *= p) ar[i] = true;
+        }
+    } 
 }
 
-int main() {
 
+int main() {
+    apowb();
     int t;
     cin >> t;
 
     while(t--) {
         int n;
         cin >> n;
-        cout << check(n) << endl;
+        printf((ar[n] ? "Yes\n" : "No\n"));
     }
 
     return 0;
