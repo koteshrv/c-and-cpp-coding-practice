4a6b6bf4d0bbe097746a1e049fd233a844704c61
Kotesh <kotesh.rv@gmail.com>
2021-02-03 12:05:28 +0530

    updated template

--- Others/Template.cpp
+++ Others/Template.cpp
@@ -1,14 +1,24 @@
+/*
+** koteshrv
+** date: ${CURRENT_DATE}/${CURRENT_MONTH}/${CURRENT_YEAR} ${CURRENT_HOUR}:${CURRENT_MINUTE}:${CURRENT_SECOND}\r
+**
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+#define gc getchar_unlocked
 #define ll long long
-#define fo(i,n) for(i=0;i<n;i++)
-#define for(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
-#define si(x)	scanf("%d",&x)
-#define sl(x)	scanf("%lld",&x)
-#define ss(s)	scanf("%s",s)
-#define pi(x)	printf("%d\n",x)
-#define pl(x)	printf("%lld\n",x)
-#define ps(s)	printf("%s\n",s)
+#define ull unsigned long long
+#define fo(i,n) for(int i = 0; i < n; i++)
+#define Fo(i,k,n) for(int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define For(i,k,n,e) for(int i = k; k < n ? i < n : i > n; k < n ? i += e : i -= e)
+#define si(x)	scanf("%d", &x)
+#define sl(x)	scanf("%lld", &x)
+#define sul(x)	scanf("%lld", &x)
+#define ss(s)	scanf("%s", s)
+#define pi(x)	printf("%d\n", x)
+#define pl(x)	printf("%lld\n", x)
+#define ps(s)	printf("%s\n", s)
 #define pb push_back
 #define mp make_pair
 #define F first
@@ -27,19 +37,41 @@ typedef vector<pl>		vpl;
 typedef vector<vi>		vvi;
 typedef vector<vl>		vvl;
 
-//===================================================================================
 
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
     int t = 1;
     cin >> t;
+
     while(t--) {
-      
+        solve();
     }
 
     return 0;
 }
 
+void solve() {
 
+}
+
+void in_vec(vi &v, int n) {
+    fo(i, n) si(v[i]);
+}
+
+void out_vec(vi v, int n) {
+    fo(i, n) pi(v[i]);
+}
+
+void in_mat(vector<vi> &v, int m, int n) {
+    fo(i, m) {
+        fo(j, n) si(v[i][j]);
+    }
+}
+
+void out_mat(vector<vi> &v, int m, int n) {
+    fo(i, m) {
+        fo(j, n) pi(v[i][j]);
+    }
+}
 
