02481de64b432d09fdb1f373f8a8291ab52923f5
Kotesh <kotesh.rv@gmail.com>
2021-04-05 20:19:43 +0530

    Practice

--- Smart_Interviews/Primary/Window_maximum.cpp
+++ Smart_Interviews/Primary/Window_maximum.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sum_of_max_window(int ar[], int n, int k) {
-    deque<int> d;
-    int sum = 0;
+void sum_of_max_window(int ar[], int n, int k) {
+    deque<long long> d;
+    long long sum = 0;
     for(int i = 0; i < n; i++) {
-        while(d.empty() && ar[i] > d.front()) {
+        while(!d.empty() && ar[i] > ar[d.back()]) {
             d.pop_back();
         }
-        if(i - k == d.front()) d.pop_front();
         d.push_back(i);
-        if(i > k - 1) sum += d.front();
+        if(i - k >= d.front()) d.pop_front();
+        if(i >= k - 1) sum += ar[d.front()];
     }
-    return sum;
+    cout << sum << endl;
 }
 
 int main() {
@@ -25,7 +25,7 @@ int main() {
         cin >> n >> k;
         int ar[n];
         for(int i = 0; i < n; i++) cin >> ar[i];
-        cout << sum_of_max_window(ar, n, k) << endl;
+        sum_of_max_window(ar, n, k);
     }
 
     return 0;
